13e2cc28d947b4b95ec0f3e490d128c151530e96
Keita Tsutsui <keitanxkeitan@gmail.com>
2014-05-15 23:14:26 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Merrymaker'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Merrymaker')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Merrymaker | Home")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Merrymaker | About Us")
+    end
+  end
+end
