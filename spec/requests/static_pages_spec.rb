05869dee589f136610fb33d481553f32f3e8c1f6
Keita Tsutsui <keitanxkeitan@gmail.com>
2014-05-16 01:00:57 +0900

    Finish exercise 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,49 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "Static pages" do
   
   subject { page }
-  let(:base_title) { "Merrymaker" }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
-
     before { visit root_path }
+    let(:heading) { 'Merrymaker' }
+    let(:page_title) { '' }
 
-    it { should have_content('Merrymaker') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About Us' }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
+    click_link "Merrymaker"
+    expect(page).to have_title(full_title(''))
   end
 end
